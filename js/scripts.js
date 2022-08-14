b533b5ec7bb36f66828d08292459d8d03fb29ae6
Matthew <mattjr4@hotmail.com>
2022-08-14 23:35:56 +0000

    Added css/directories

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,20 @@ var request = new XMLHttpRequest()
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload = function () {
   // Begin accessing JSON data here
-  data.forEach(movie => {
-    //Log each movie's title
-    console.log(movie.title)
-
-  })
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      console.log(movie.title)
+    })
+  } else {
+    console.log('error')
+  }
 }
-
-//Send request
 request.send()
 
-// Begin accessing JSON data here
-var data = JSON.parse(this.response)
+const app = document.getElementById('root')
+
+
 
 
 
